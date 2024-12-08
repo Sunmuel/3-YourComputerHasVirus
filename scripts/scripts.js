428a98b9e8ec5658d4b6d43dca1825f8a595f439
Samuel <samuelbaraujo19@gmail.com>
2024-12-08 18:33:19 -0300

    Removing every time's code from styles.js and adding it to the new file: time.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,6 @@
-const time = document.getElementById("time");
-
-function updateTime() {
-    const now = new Date();
-    let hours = now.getHours();
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    let amPm = "PM";
-
-    hours = hours > 12? hours % 12 : hours;
-    hours = hours ? hours : 12;
-
-    if (hours === 12) {
-        if (amPm === "PM") {
-            amPm = "AM";
-        } else {
-            amPm = "PM";
-        }
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'c') {
+      event.preventDefault();
+      alert('Copying is disabled on this website.');
     }
-
-    time.innerHTML = `<button id="time-button">${hours}:${minutes} ${amPm}</button>`
-}
-updateTime();
-setInterval(() => {
-    updateTime();
-}, 1000);
+  });
