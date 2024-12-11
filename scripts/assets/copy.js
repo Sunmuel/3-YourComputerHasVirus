7e60d8a5549d8475670f11efb75e41ba68bf4d30
Samuel <samuelbaraujo19@gmail.com>
2024-12-11 19:52:52 -0300

    Bringing together time.js, copy.js and every other asset that will be used later

---
+++ scripts/assets/copy.js
@@ -0,0 +1,11 @@
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "c") {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "x") {
+    event.preventDefault();
+  }
+});
