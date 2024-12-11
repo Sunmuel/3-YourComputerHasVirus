f3ba5f968523ab35694358d6baa65279de11e59d
Samuel <samuelbaraujo19@gmail.com>
2024-12-11 19:55:56 -0300

    Creating a setInterval that will show a new error popup every 1 second ( for now ) and trying to display the error popups wherever I want

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,26 @@
-document.addEventListener('keydown', function (event) {
-    if (event.ctrlKey && event.key === 'c') {
-      event.preventDefault();
-      alert('Copying is disabled on this website.');
+let virusPopup = document.getElementById("a");
+let textarea = document.getElementById("textarea");
+
+setInterval(() => {
+  virusPopup.innerHTML = `
+    <div class="virus-popup" id="grid-area-3456">
+      <img src="images/popup.png" alt="Windows 95 popup image">
+      <h3>ERROR 999</h3>
+      <button id="ok-button">
+        <img src="images/ok.png" alt="Windows 95's OK button when a popup appears">
+      </button>
+    </div>
+    `;
+}, 1000);
+
+textarea.addEventListener("input", () => {
+  let value = textarea.value.trim();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      if (value === "banana.") {
+        console.log("boa");
+      }
     }
   });
+});
